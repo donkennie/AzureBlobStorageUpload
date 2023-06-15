6a239966f258d3f393326cac3df0de2ba9914f32
donkennie <ajeigbekehinde160@gmail.com>
2023-06-15 10:34:03 +0100

    Add project files.

---
+++ AzureBlobStorageUpload/Program.cs
@@ -0,0 +1,15 @@
+﻿using Azure.Storage.Blobs;
+
+Console.WriteLine("Starting...");
+
+var blobStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=blobstoragetodo;AccountKey=Edn+R/jwmMkva527z7R5TyXdHbvdq8v3NkTwaqVQ6dW93Uoy3c84LVuT7DVLspK01YH8EK4fdTEc+ASt32oSQg==;EndpointSuffix=core.windows.net";
+var blobStorageContainerName = "uploadfile";
+
+var container = new BlobContainerClient(blobStorageConnectionString, blobStorageContainerName);
+var blob = container.GetBlobClient("kennie.jpeg");
+
+var stream = File.OpenRead("kennie.jpeg");
+await blob.UploadAsync(stream); 
+
+
+Console.WriteLine("Upload Completed!");
